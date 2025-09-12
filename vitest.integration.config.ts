531bcc0a1372b3e081675d3ce9827e88425761a1
adam goldsmith <adam.goldsmith0@gmail.com>
2025-09-12 11:40:38 +0100

    integration testing config

---
+++ vitest.integration.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/cli.integration.test.ts'],  // only include your integration tests
+    exclude: ['node_modules/**']
+  }
+});
